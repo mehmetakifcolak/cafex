c3c80d29adae639bde2127306fc753b3f5f1b070
mymac <mymac@192.168.1.33>
2024-07-28 22:04:52 +0300

    Initial commit

---
+++ myapp/urls.py
@@ -0,0 +1,11 @@
+# myapp/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import MyModelViewSet
+
+router = DefaultRouter()
+router.register(r'mymodels', MyModelViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
